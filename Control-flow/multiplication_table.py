abe3648d241a55d5cf8da0e8e67bd8334c692f55
retha-n <22retha@gmail.com>
2025-06-23 00:37:29 +0200

    Delete Control-flow directory

--- Control-flow/multiplication_table.py
+++
@@ -1,4 +0,0 @@
-number = str(input("Enter a number to see its multiplication table:"))
-for i in range(1,11):
-  print(number "*" i = number*i)
-  
--- Control-flow/pattern_drawing.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter the size of the pattern:"))
-row = 0
-while row < number:
-    for i in range(number):
-        print("*", end= "")
-    print()
-    row +=1
