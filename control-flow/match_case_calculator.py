884120ca72d2758638bd050dab981b8d2ebb2a9c
retha-n <22retha@gmail.com>
2025-06-30 15:33:19 +0200

    Update match_case_calculator.py uder prompt

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-operation = input("What type of operation would you like to perform?")
+operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
     case '+':
