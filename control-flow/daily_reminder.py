f48843c0133c6723960f41a85f44ba9ca751e054
retha-n <22retha@gmail.com>
2025-06-30 15:17:53 +0200

    Update daily_reminder.py user prompt typo

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,4 +1,4 @@
-task = str(input("Enter yoyr task:"))
+task = str(input("Enter your task:"))
 priority = str(input("Priority (high/medium/low):"))
 time_bound = str(input("Is it time-bound? (yes/no):"))
 
